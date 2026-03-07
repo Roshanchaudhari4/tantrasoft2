4740c461563523d852743e0bd7a9b383c0961f6e
Anurag9015 <anurag.p@tantrasoftsolutions.com>
2026-03-07 14:52:46 +0530

    second commit

--- arrow.js
+++ arrow.js
@@ -3,3 +3,50 @@ var cars = ["hmi", 160, "hmi2", "150"];
 let index = cars.findIndex(num => num == "150")
 
 console.log(cars[index])
+
+
+
+// var arr =["bmw", 150, "ppp"]
+
+// let result = arr.findIndex(num => num == 150)
+
+// console.log(arr[result])
+
+
+
+
+
+
+// let result = (a,b) => (a * b)
+
+// console.log(result(2,3))
+
+
+
+
+
+
+// const numbers = [1,2,3];
+
+// numbers.map(num => num * 2);
+
+// console.log(numbers)
+
+
+
+
+
+
+// const obj = {
+//   name: "ABC",
+
+//   show: function() {   //  outer function
+//     const arrow = () => {   //  arrow function
+//       console.log(this.name);
+//     }
+
+//     arrow();
+//   }
+// }
+
+// obj.show();
